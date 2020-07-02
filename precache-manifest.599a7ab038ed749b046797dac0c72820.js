7e1fd27b0b47822de124d095ff20a55115321a32
Bruno Viveiros <bruferviveiros@gmail.com>
2020-07-02 09:20:54 -0300

    Updates

---
+++ precache-manifest.599a7ab038ed749b046797dac0c72820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a0e0c899081274f01dc644bfa35dc0",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "0213e4e856f7e8198651",
+    "url": "/github-explorer/static/js/2.ed4b1f82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.ed4b1f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc4a6f8de8eea886e83",
+    "url": "/github-explorer/static/js/main.161d7d2f.chunk.js"
+  },
+  {
+    "revision": "6bdb3542f7c98cf05f87",
+    "url": "/github-explorer/static/js/runtime-main.307ea7e1.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
